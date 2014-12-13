fc24c9199d7b28ae3c4931da96ce58cd5447d0f0
Jules Briquet <jules.briquet@gmail.com>
2014-12-13 16:19:55 +0100

    Improve/Fix spawn system + Multiple prefabs for asteroids

---
+++ Game/Assets/Scripts/Global/BadRessourceHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BadRessourceHandler : MonoBehaviour
+{
+
+    public int lifeTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, lifeTime);
+    }
+
+}
