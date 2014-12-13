e546844b9190cb25437974ecab9fd382e2710218
Jules Briquet <jules.briquet@gmail.com>
2014-12-13 11:21:32 +0100

    First project hierarchy and samples

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    int playerNumber = 1;
+    public Vector2 speed;
+    public Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
