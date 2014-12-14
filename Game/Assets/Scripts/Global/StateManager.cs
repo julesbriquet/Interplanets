790709bbc1c26e9fb4d4c80a5a9985f6f6500cb1
Loïc Huck <contact@huckloic.fr>
2014-12-14 03:07:28 +0100

    Main menu

--- Game/Assets/Scripts/Global/StateManager.cs
+++ Game/Assets/Scripts/Global/StateManager.cs
@@ -4,5 +4,8 @@ using System.Collections;
 public static class StateManager{
 	
 	public static int numberOfPlayers = 1;
-		
+
+	public static void Play(){
+		Debug.Log ("Click");
+	}
 }
---
+++ Game/Assets/Scripts/UI/UIMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIMainMenu : MonoBehaviour {
+
+	public void StartGame(){
+		Application.LoadLevel("main_scene");
+	}
+
+	public void HowToPlay(){
+		//Application.LoadLevel("main_scene");
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
