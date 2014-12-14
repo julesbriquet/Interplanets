076b8d7254734625282ba2a597f9f11ae373fc0a
Loïc Huck <contact@huckloic.fr>
2014-12-14 06:22:02 +0100

    Ship movement test improvements

---
+++ Game/Assets/Scripts/UI/UIFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIFollow : MonoBehaviour {
+
+	public Transform target;
+
+	public float force = 0.5f;
+	public float speed = 3f;
+
+	private Vector3 velocity = new Vector3();
+
+	void LateUpdate(){
+		Vector3 destination = target.transform.position - transform.position;
+		destination.z = 0f;
+			
+		Vector3 steering = destination - velocity;
+		steering = Vector3.ClampMagnitude(steering, force);
+		velocity = Vector3.ClampMagnitude(velocity + steering, speed);
+
+		transform.position = transform.position + velocity * Time.deltaTime;
+	}
+}
--- Game/Assets/ShipMovementTest.cs
+++ Game/Assets/ShipMovementTest.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class ShipMovementTest : MonoBehaviour {
 	public Transform engine;
 	public float speed = 3f;
-	public float turbulance = 0.25f;
 	public float force = 0.1f;
-	public Vector2 destination = new Vector2();
-	public Vector2 velocity = new Vector2();
+
+	public float turbulanceForce = 1f;
+	public float turbulanceFrequence = 1f;
+
+	public float stunTime = 1f;
+
+	private float stun = 0f;
+	private float timePassed = 0f;
+
+	private Vector2 destination = new Vector2();
+	private Vector2 velocity = new Vector2();
+	private Vector2 turbulance = new Vector2();
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -19,13 +28,25 @@ public class ShipMovementTest : MonoBehaviour {
 		float inputX = Input.GetAxis("P1_Horizontal");
 		float inputY = Input.GetAxis("P1_Vertical");
 
-		destination = new Vector2(inputX, inputY).normalized * speed;
+		if(Input.GetKey(KeyCode.Space) && stun == 0f){
+			stun = 0.0001f;
+			velocity = -velocity.normalized * 15f;
+		}
+
+		if(stun > stunTime){
+			stun = 0f;
+		} else if(stun != 0f){
+			stun += Time.deltaTime;
+			destination = new Vector2();
+		} else {
+			destination = new Vector2(inputX, inputY).normalized * speed;
+		}
 	}
 
 	void FixedUpdate(){
 
 		Vector2 steering = destination - velocity;
-		steering = Vector2.ClampMagnitude(steering, force) + (new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * turbulance);
+		steering = Vector2.ClampMagnitude(steering, force) + AddTurbulances();
 		
 		velocity = velocity + Vector2.ClampMagnitude(steering, speed);
 		
@@ -37,5 +58,16 @@ public class ShipMovementTest : MonoBehaviour {
 		transform.localEulerAngles = new Vector3(-velocity.y * 3f, transform.localEulerAngles.y, velocity.y * 3f);
 
 		rigidbody2D.MovePosition(rigidbody2D.position + velocity * Time.deltaTime);
+	}
+
+	Vector2 AddTurbulances(){
+		timePassed += Time.deltaTime;
+		if(timePassed > turbulanceFrequence){
+			timePassed = 0f;
+			turbulance = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * turbulanceForce;
+		} else {
+			turbulance = new Vector2();
+		}
+		return turbulance;
 	}
 }
